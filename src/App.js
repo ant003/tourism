972cfa5c799eb101a292da7569aab4bb58c25b24
ant003 <antonio.alvarez.chavarria@gmail.com>
2022-11-17 15:53:28 -0600

    Navbar and footer text centering issue fixed

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ function App() {
     return (
         <div className="App">
             <Navbar/>
-            <div className="content">
-                <SquareList/>
-            </div>
+            <SquareList/>
             <Footer/>
         </div>
 
