f285d6919895bcc625505b3c66d0b3abba8766b0
ant003 <antonio.alvarez.chavarria@gmail.com>
2022-11-29 10:47:56 -0600

    The <a> tags were replaced with the <Link> tag from react router

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,12 +1,14 @@
+import { Link } from 'react-router-dom';
+
 const Navbar = () => {
     return (
         <nav className="navbar">
             <ul>
-                <li><a href="/">Inicio</a></li>
-                <li><a href="/lugares"> Lugares</a> </li>
-                <li><a href="/mapa">Mapa</a></li>
-                <li><a href="/consejos">Consejos</a></li>
-                <li><a href="/contacto">Contacto</a></li>
+                <li><Link to="/">Inicio</Link></li>
+                <li><Link to="/lugares"> Lugares</Link> </li>
+                <li><Link to="/mapa">Mapa</Link></li>
+                <li><Link to="/consejos">Consejos</Link></li>
+                <li><Link to="/contacto">Contacto</Link></li>
             </ul>
         </nav>
     );
