9012ef0a6bc1461e151ce7ed6f64fc939edb7838
ant003 <antonio.alvarez.chavarria@gmail.com>
2022-12-09 10:56:35 -0600

    The new Place component was added as well as its respective assets and styles

--- src/Place.js
+++ src/Place.js
@@ -1,10 +1,29 @@
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 
 const Place = () => {
     const { id } = useParams();
 
+    const { data:place, isLoading, error} = useFetch(`http://localhost:8000/places/${id}`);
+
     return(
-        <h1>Place - { id }</h1>
+        <div className="place">
+            {error && <div>{error}</div>}
+            {isLoading && <div>Loading...</div>}
+            <div className="banner">
+                <div className="bannerCont">
+                    <h1>{ place && place.name }</h1>
+                    <p>Test</p>
+                </div>
+            </div>
+            <div className="content">
+                <p><strong>Distancia:</strong> { place && place.distance}</p>
+                <p><strong>Difcultad:</strong> { place && place.difficulty}</p>
+                <p><strong>Vehículo:</strong> { place && place.vehicle}</p>
+                <p><strong>Precio:</strong> { place && place.price}</p>
+                <p><strong>Descripción:</strong> { place && place.description}</p>
+            </div>
+        </div>
     );
 }
 
