cdb6f2ba810ed4d37ee85425c77871c337697348
ant003 <antonio.alvarez.chavarria@gmail.com>
2023-02-02 09:15:17 -0600

    All the images needed for display in the place list were added and now the place list component display each one of them to its corresponding place

--- src/Places/Square.js
+++ src/Places/Square.js
@@ -1,9 +1,14 @@
-import noImage from "../img/noImage.png";
-
 const Square = ({square}) =>{
+
+   let imageName = "noImage.png";
+   
+    if(square.imageName !== undefined){
+        imageName = square.imageName;
+    }
+
     return (
         <div className="square">
-            <img src={noImage} alt="noImage"></img>
+            <img src={require(`../img/${imageName}`)} alt="noImage"></img>
             <h2>{square.name}</h2>
             <p>{`${square.experience} + ${square.vehicle} + ${square.price}` }</p>
         </div>
