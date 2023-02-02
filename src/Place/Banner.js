d35803494f398a3913f105ece72539427fd767aa
ant003 <antonio.alvarez.chavarria@gmail.com>
2023-02-02 14:35:56 -0600

    The banner of each place displays the corresponding image for each one of them

--- src/Place/Banner.js
+++ src/Place/Banner.js
@@ -1,9 +1,20 @@
 import KeyList from "./KeyList";
+// import landscape from "../img/lanscape.jpg"
 
 const Banner = ({nameList,keyList,dataList}) => {
+
+    let imageName = "lanscape.jpg";
+   
+    if(dataList.imageName !== undefined){
+        imageName = dataList.imageName;
+    }
+
     return(
             <div className="banner">
-            <div className="bannerCont">
+                <div className="background">
+                    <img src={require(`../img/${imageName}`)} alt="banner-background" />
+                </div>
+            <div className="bannerCont">   
                 <h1>{ dataList.name }</h1>
                 <h2>{dataList.province}</h2>
                 <ul>
