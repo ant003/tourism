e4ee1bce14486902614e5f2ee73fc9538e6adbc9
ant003 <antonio.alvarez.chavarria@gmail.com>
2023-02-10 09:33:17 -0600

    Carousel component added with its CSS file

---
+++ src/Home/Carousel.js
@@ -0,0 +1,52 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+const Carousel = ({ children, size }) => {
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [length, setLength] = useState(children.length);
+
+    // Set the length to match current children from props
+    useEffect(() => {
+        setLength(children.length);
+    }, [children]);
+
+    const handleNext = () => {
+        if (currentIndex < length - size) {
+            setCurrentIndex(prev => prev + 1);
+        }
+    }
+
+    const handlePrev = () => {
+        if (currentIndex > 0) {
+            setCurrentIndex(prev => prev - 1);
+        }
+    }
+
+    return (
+        <div className="carousel">
+            <div className="carousel-wrapper">
+                {
+                    currentIndex > 0 &&
+                    <button onClick={handlePrev} className="left-arrow">
+                        &lt;
+                    </button>
+                }
+                <div className="carousel-content-wrapper">
+                    <div className={`carousel-content show-${size}`}
+                        style={{ transform: `translateX(-${currentIndex * (100 / size)}%)` }}
+                    >
+                        {children}
+                    </div>
+                </div>
+                {
+                    currentIndex < (length - size) &&
+                    <button onClick={handleNext} className="right-arrow">
+                        &gt;
+                    </button>
+                }
+            </div>
+        </div>
+    )
+}
+
+export default Carousel;
--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,22 +1,28 @@
 import noImage from '../img/noImage.png'
+import Carousel from './Carousel';
+import './carousel.css';
 
 const Home = () => {
-    return(
+    return (
         <div className="home">
             <section id="standOuot">
                 <h1>Lugar destacado</h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt mollitia a aut soluta, ipsam recusandae esse magni non, iste modi quidem nemo quas repudiandae itaque, reiciendis est aspernatur delectus labore.</p>
             </section>
             <section id="standPhoto">
-                <img src={noImage} alt="" min_width="500px"/>
+                <img src={noImage} alt="" min_width="500px" />
             </section>
             <section id="newPlaces">
                 <h2>Lugares nuevos</h2>
-                <div className="img">
+                <Carousel size={3}>
                     <img src={noImage} alt="" />
                     <img src={noImage} alt="" />
                     <img src={noImage} alt="" />
-                </div>
+                    <img src={noImage} alt="" />
+                    <img src={noImage} alt="" />
+                    <img src={noImage} alt="" />
+                    <img src={noImage} alt="" />
+                </Carousel>
             </section>
         </div>
     );
